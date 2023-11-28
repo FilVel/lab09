7fa466b45933c2567394c7ca5722d12b814cd87e
Filippo Velli <filippo.velli@studio.unibo.it>
2023-11-28 11:38:26 +0100

    SimpleGUI has been fully implemented

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,18 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.LayoutManager;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +21,42 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+
+    private SimpleGUI(Controller controller) {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final JTextArea text = new JTextArea();
+        final JPanel panel = new JPanel();
+        final LayoutManager layout = new BorderLayout();
+        panel.setLayout(layout);
+
+        final JButton save = new JButton("Save");
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent event) {
+                try {
+                    controller.write(text.getText());
+                } catch (IOException e) {
+                    JOptionPane.showMessageDialog(null, e.getMessage(), "Error!!!", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        panel.add(save, BorderLayout.SOUTH);
+        panel.add(text, BorderLayout.CENTER);
+        frame.setContentPane(panel);
+
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize((int) screen.getWidth() / 4, (int) screen.getHeight() / 4);
+        frame.setLocationByPlatform(true);
+    }
+
+    private void display(){
+        this.frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        final SimpleGUI gui = new SimpleGUI(new Controller());
+        gui.display();
+    }
 
 }
